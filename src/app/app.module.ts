c5dabf4b9a0377767d4372d4d59744a93c9958df
grigorjev.ilja@gmail.com <werbatim58803110>
2020-12-17 16:52:57 +0200

    Folder set-up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import { HomeComponent } from './home/home.component';
+import { ItemAddComponent } from './item-add/item-add.component';
+import { ItemProductComponent } from './item-product/item-product.component';
+import { AddProductComponent } from './add-product/add-product.component';
+import { ProductViewComponent } from './product-view/product-view.component';
+import { ProductEditComponent } from './product-edit/product-edit.component';
+import { NavbarComponent } from './global/navbar/navbar.component';
+import { FooterComponent } from './global/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ItemAddComponent,
+    ItemProductComponent,
+    AddProductComponent,
+    ProductViewComponent,
+    ProductEditComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
